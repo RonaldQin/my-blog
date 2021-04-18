1d74bc4b8538096ba759d11bdb3594b92ab97d24
lace <ronaldqinbiao@gmail.com>
2021-04-18 15:05:07 +0800

    前端查询。

--- src/main/java/com/example/demo/dao/BlogRepository.java
+++ src/main/java/com/example/demo/dao/BlogRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,4 +13,7 @@ import com.example.demo.po.Blog;
 public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificationExecutor<Blog> {
 	@Query("select b from Blog b where b.recommend = true")
 	List<Blog> findTop(Pageable pageable);
+	
+	@Query("select b from Blog b where b.title like ?1 or b.content like ?1")
+	Page<Blog> findByQuery(String query, Pageable pageable);
 }
--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -7,6 +7,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.service.BlogService;
 import com.example.demo.service.TagService;
@@ -30,6 +33,14 @@ public class IndexController {
 		model.addAttribute("tags", tagService.listTagTop(10));
 		model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
 		return "index";
+	}
+	
+	@PostMapping("/search")
+	public String search(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+			@RequestParam String query, Model model) {
+		model.addAttribute("page", blogService.listBlog("%"+query+"%", pageable));
+		model.addAttribute("query", query);
+		return "search";
 	}
 	
 	@GetMapping("/blog")
